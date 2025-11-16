de5395b5bdeaa123c2bcd66015335e6db5873808
Yianni <yianni@matteranalytics.io>
2025-11-16 16:30:15 -0600

    Add AASA file for Universal Links

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
